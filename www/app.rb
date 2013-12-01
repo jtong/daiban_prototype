f509254923943fafd6b3fb1d78423048ca678655
jtong <tj19832@gmail.com>
2013-12-01 17:18:52 +0800

    jtong - init

---
+++ www/app.rb
@@ -0,0 +1,30 @@
+ENV['RACK_ENV'] ||= "development"
+
+require 'sinatra'
+require 'haml'
+require 'moode-haml-toolkit/component_helper'
+require 'jt-partial'
+if File.exist? "component/custom.rb"
+  load "component/custom.rb"
+end
+require 'moode-haml-toolkit/component_generator'
+
+require 'moode-haml-component-angular'
+
+
+configure do
+  set :views, File.join(File.dirname(__FILE__), 'hamls')
+  set :public_folder, File.dirname(__FILE__)
+end
+
+get "/" do
+  haml :index
+end
+
+get "/pages/:name.html" do
+  haml "pages/#{params[:name]}".to_sym
+end
+
+get "/:page_name.html" do
+  haml params[:page_name].to_sym
+end
---
+++ www/legacy_js/routes.js
@@ -0,0 +1,23 @@
+myModule.config(function($routeProvider) {
+
+
+    //routing generate
+    //routing generated over
+});
+
+/** Here is example
+myModule.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "pages/activity_list_page.html",
+        controller: ActivityListController
+    }).when("/activity/create", {
+            templateUrl: "pages/activity_create_page.html",
+            controller: ActivityCreateController
+        }).when("/sign_ups/list/:activity_name", {
+            templateUrl: "pages/apply_page.html",
+            controller: SignUpListController
+        }).otherwise({
+            redirectTo: "/"
+        });
+});
+**/
